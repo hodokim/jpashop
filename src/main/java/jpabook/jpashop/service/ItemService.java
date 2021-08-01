f67736bd67f1e4950ebc695bab1ded293da7c4e5
hodo323 <hodo323@naver.com>
2021-08-01 19:31:16 +0900

    JPA Study Day 20

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,16 @@ public class ItemService {
      @Transactional
      public void saveItem(Item item){
          itemRepository.save(item);
+     }
+
+     @Transactional
+     public Item  updateItem(Long itemId, String name, int price, int stockQuantity ){
+         Item findItem = itemRepository.findOne(itemId);
+         findItem.setName(name);
+         findItem.setPrice(price);
+         findItem.setStockQuantity(stockQuantity);
+
+         return findItem;
      }
 
      public List<Item> findItems(){
